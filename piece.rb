5b9be1cfd50c189600a7cdc8e6c9f13fc5ad6c3b
sigma29 <litagaki@gmail.com>
2015-08-06 08:45:53 -0400

    Create Piece#move_diffs

--- piece.rb
+++ piece.rb
@@ -2,6 +2,18 @@ require 'byebug'
 require_relative 'board'
 
 class Piece
+  UP_DELTAS = [
+    [-1, 1],
+    [ 1, 1]
+  ]
+  DOWN_DELTAS = [
+    [ 1,-1],
+    [-1,-1]
+  ]
+  PIECE_DELTAS = {
+    :red => UP_DELTAS,
+    :black => DOWN_DELTAS
+  }
   attr_reader :color, :board
   attr_accessor :position
   attr_writer :is_king
@@ -13,5 +25,13 @@ class Piece
 
   def is_king?
     @is_king
+  end
+
+  def move_diffs
+    if is_king?
+      UP_DELTAS + DOWN_DELTAS
+    else
+      PIECE_DELTAS[color]
+    end
   end
 end
