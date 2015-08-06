b6fde6cbeeb89645b0272e177a7d1e24a6c6e3e5
sigma29 <litagaki@gmail.com>
2015-08-06 13:40:53 -0400

    Add Board#setup

--- board.rb
+++ board.rb
@@ -3,6 +3,14 @@ require_relative 'piece'
 
 class Board
   BOARD_SIZE = 8
+  COLOR_ROWS = {
+    :red => [ 0, 1, 2],
+    :black => [5, 6, 7]
+  }
+  CHECKER_POSITIONS = [
+    [1, 3, 5, 7],
+    [0, 2, 4, 6]
+  ]
   def self.on_board?(pos)
     pos.all? { |coordinate| coordinate.between?(0, BOARD_SIZE - 1) }
   end
@@ -11,6 +19,7 @@ class Board
 
   def initialize(setup = false)
     @grid = Array.new(BOARD_SIZE) { Array.new(BOARD_SIZE) }
+    setup_board if setup
   end
 
   def [](pos)
@@ -57,6 +66,24 @@ class Board
   def can_capture?(color)
     color_pieces = pieces.select {|piece| piece.color == color}
     color_pieces.any? {|piece| piece.can_capture?}
+  end
+
+  def setup_board
+    COLOR_ROWS.each do |color, rows|
+      rows.each do |row|
+        if row % 2 == 0
+          checker_positions = CHECKER_POSITIONS[0]
+        else
+          checker_positions = CHECKER_POSITIONS[1]
+        end
+        checker_positions.each do |column|
+          position = [column, row]
+          self[position] = Piece.new(color, position, self)
+        end
+      end
+    end
+
+    self
   end
 
   def render
