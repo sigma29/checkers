6ea1e1a4d8fc32397fe02941e748422ab9f5adb0
sigma29 <litagaki@gmail.com>
2015-08-06 08:31:02 -0400

    Create Piece class

---
+++ piece.rb
@@ -0,0 +1,16 @@
+require 'byebug'
+
+class Piece
+  attr_reader :color, :board
+  attr_accessor :position
+  attr_writer :is_king
+
+  def initialize(color,position,board)
+    @color, @position, @board = color, position, board
+    @is_king = false
+  end
+  
+  def is_king?
+    @is_king
+  end
+end
