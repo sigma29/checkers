b40466333d33522b48bba2fd85ed3d4fc730794f
sigma29 <litagaki@gmail.com>
2015-08-06 14:37:42 -0400

    Make Checkers#play method with game outline

--- checkers.rb
+++ checkers.rb
@@ -10,9 +10,32 @@ class Checkers
     @board = Board.new(true)
     @red = HumanPlayer.new(:red)
     @black = HumanPlayer.new(:black)
-    @current_player = black_player
+    @current_player = @black
   end
 
+  def play
+
+    until board.lost?(current_player.color)
+      board.render
+      take_turn
+      swap_player
+    end
+
+    puts "#{opponent.color.upcase} won!"
+  end
+
+  private
+
+  def take_turn
+    begin
+      move_sequence = current_player.move_input
+      puts "Move sequence: #{move_sequence}"
+      board.make_moves(move_sequence)
+    rescue InvalidMoveError => e
+      puts e.message
+      retry
+    end
+  end
 
   def opponent
     (current_player == red) ? :black : :red
@@ -21,6 +44,7 @@ class Checkers
   def swap_player
     self.current_player = (current_player == red) ? :black : :red
   end
+
 end
 
 class HumanPlayer
@@ -30,4 +54,8 @@ class HumanPlayer
     @color = color
   end
 
+  def move_input
+    puts "Enter your desired move sequence"
+    gets.chomp
+  end
 end
